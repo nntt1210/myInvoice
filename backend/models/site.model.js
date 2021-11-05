66353b96e9c7ef86371bd3579ba9759dfd2dd1e4
Văn Đình Khôi <63282078+khoivankarik@users.noreply.github.com>
2021-11-05 09:55:55 +0700

    add statistics query

--- backend/models/site.model.js
+++ backend/models/site.model.js
@@ -4,13 +4,38 @@ exports.all = () => {
   return db.load("select * from SanPham");
 };
 
+exports.statistics = () => {
+  return db.load(`
+  ;WITH months(MonthNumber,nam,doanhthu) AS
+  (
+      select distinct 1,year(HD.NgayLap),0
+    from HoaDon HD
+      UNION ALL
+      SELECT MonthNumber+1,nam,0
+      FROM months
+      where MonthNumber < 12
+  ),
+    temp(Month,YEAR,DOANHTHU) as
+  (
+    SELECT Month(HD.NgayLap) as month,YEAR(HD.NgayLap) as year,sum(HD.TongTien) as DoanhThu
+    FROM HoaDon HD
+    GROUP BY Month(HD.NgayLap),YEAR(HD.NgayLap)
+    --ORDER BY YEAR(HD.NgayLap) DESC,Month(HD.NgayLap) DESC
+  )
+  select M.MonthNumber as Month,M.nam as Year, ISNULL(T.DOANHTHU,0) as DoanhThu
+  from months M
+  left join temp T
+  on M.nam=T.YEAR and M.MonthNumber=T.Month
+  order by M.nam ASC, M.MonthNumber ASC`);
+};
+
 // exports.single = (id) => {
 //   return db.load(`select * from SanPham where MaSP = ${id}`);
 // };
 
-// exports.add = (entity) => {
-//   return db.add("SanPham", entity);
-// };
+exports.add = (entity) => {
+  return db.add("SanPham", entity);
+};
 
 // exports.del = (id) => {
 //   return db.del("SanPham", "MaSP", id);
